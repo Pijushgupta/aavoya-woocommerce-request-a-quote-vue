632fc1cc1f82e994c390ade82f77080db1e3e109
Pijush gupta <pijush@live.com>
2022-08-08 18:00:09 +0530

    Mapping done

--- awraq/Frontend/Form/Action/Map.php
+++ awraq/Frontend/Form/Action/Map.php
@@ -22,38 +22,45 @@ class Map {
 
 		$sanitizedPostData = array();
 		foreach ($post as $key => $input) {
+
 			$inputArray = explode('_', $key);
-			//echo $inputArray[0];
+
 			foreach ($formMeta as $inputMeta) {
 				if ($inputMeta['uniqueName'] == $inputArray[0]) {
-
-					if (!in_array($inputMeta['uniqueName'], $sanitizedPostData)) {
-						array_push($sanitizedPostData, array($inputMeta['uniqueName'] => array())); //creating new array element to hold data
-					}
-
-
 					if (count($inputArray) > 1) {
 						if ($inputMeta['type'] == 'name') { //name type element 
 							if ($inputMeta['data']['Options'][$inputArray[1]]['enabled'] != 1) return false; //Terminate everything - Possible hacking attempt 
 							if ($inputMeta['data']['Options'][$inputArray[1]]['required'] == 1) { //checking if the field is required 
 								if (empty($input) and $input != 0) return false; //if field is is required but with no data
 							}
-							sanitize_text_field($input);
+							$sanitizedPostData[$inputMeta['uniqueName']][$inputArray[1]]['name'] = $inputMeta['data']['Options'][$inputArray[1]]['name'];
+							$sanitizedPostData[$inputMeta['uniqueName']][$inputArray[1]]['data'] = sanitize_text_field($input);
 						}
 
 						if ($inputMeta['type'] == 'checkbox') { // checkbox type element 
 
+							if ($inputMeta['data']['Options'][$inputArray[1]]['required'] == 1) { //checking if the field is required 
+								if (empty($input) and $input != 0) return false; //if field is is required but with no data
+							}
+							$sanitizedPostData[$inputMeta['uniqueName']][$inputArray[1]]['name'] = $inputMeta['data']['Options'][$inputArray[1]]['name'];
+							$sanitizedPostData[$inputMeta['uniqueName']][$inputArray[1]]['data'] = sanitize_text_field($input);
 						}
 
 						if ($inputMeta['type'] == 'address') { // address type element 
-
+							if ($inputMeta['data']['Options'][$inputArray[1]]['enabled'] != 1) return false; //Terminate everything - Possible hacking attempt 
+							if ($inputMeta['data']['Options'][$inputArray[1]]['required'] == 1) { //checking if the field is required 
+								if (empty($input) and $input != 0) return false; //if field is is required but with no data
+							}
+							$sanitizedPostData[$inputMeta['uniqueName']][$inputArray[1]]['name'] = $inputMeta['data']['Options'][$inputArray[1]]['name'];
+							$sanitizedPostData[$inputMeta['uniqueName']][$inputArray[1]]['data'] = sanitize_text_field($input);
 						}
-						echo '<pre>';
-						print_r($inputArray);
-						echo '</pre>';
+					} else {
+						$sanitizedPostData[$inputMeta['uniqueName']][$inputArray[1]]['name'] = $inputMeta['name'];
+						$sanitizedPostData[$inputMeta['uniqueName']][$inputArray[1]]['data'] = sanitize_text_field($input);
 					}
 				}
 			}
 		}
+		var_dump($sanitizedPostData);
 	}
 }
