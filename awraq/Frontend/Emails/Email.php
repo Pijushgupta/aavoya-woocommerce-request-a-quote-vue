38a398baab3de8c59e6afcaeb18dd317e1432039
Pijush gupta <pijush@live.com>
2022-09-12 16:13:06 +0530

    wordking on notification sending

--- awraq/Frontend/Emails/Email.php
+++ awraq/Frontend/Emails/Email.php
@@ -3,6 +3,8 @@ namespace Awraq\Frontend\Emails;
 if(!defined('ABSPATH')) exit;
 
 class Email {
+
+	public static $formData;
 	/**
 	 * @param int $formID
 	 * @param array $formData
@@ -10,47 +12,66 @@ class Email {
 	 * @return bool
 	 */
 	public static function init(int $formID,array $formData):bool{
-		self::sendAdminNotification($formID,$formData);
-		self::sendUserNotification($formID,$formData);
+		self::$formData = $formData;
+		//self::sendAdminNotification($formID);
+		self::sendUserNotification($formID);
 		return true;
 	}
-	public static function transalate(){
+	public static function transalate($setting_element){
+		/**
+		 * checking this pattern -  {anything}
+		 */
+		$intStatus = preg_match_all("/\{ .* \}/im",$setting_element,$output);
+		/**
+		 * if no pattern found return the data
+		 */
+		if($intStatus == 0) return $setting_element;
 
+		/**
+		 * if pattern found, trim {,}
+		 */
+		$output = trim($output[0][0],'{}');
+
+		if($output == 'all'){
+
+		}
 	}
 	public static function sendAdminNotification(int $formID,array $formData){
 		$adminNotificationSettings = unserialize(get_post_meta($formID,'awraqFormAdminNotification',true));
 		if($adminNotificationSettings['en'] === false) return false;
 
 	}
-	public static function sendUserNotification(int $formID,array $formData){
+	public static function sendUserNotification(int $formID) {
 		/**
 		 * getting user email notification setting from form meta
 		 */
-		$userNotificationSettins = unserialize(get_post_meta($formID,'awraqFormUserNotification',true));
+		$userNotificationSettins = unserialize( get_post_meta( $formID, 'awraqFormUserNotification', true ) );
 		/**
 		 * checking if it is enabled or not
 		 */
-		if($userNotificationSettins['en'] === false) return false;
+		if ( $userNotificationSettins['en'] === false ) {
+			return false;
+		}
 
 		/**
 		 * unsetting, since its not required anymore
 		 */
-		unset($userNotificationSettins['en']);
+		unset( $userNotificationSettins['en'] );
+		$newAarray = array();
 
-		foreach($userNotificationSettins as $setting){
-			if($setting['sent_to_email']){
-				$sent_tos = explode(',',$setting['sent_to_email']);
-				foreach($sent_tos as $sent_to){
-					
+		foreach ( $userNotificationSettins as $key => $setting ) {
+			
+				$setting_elements = explode( ',', $setting );
+				$count = 0;
+				foreach ( $setting_elements as $setting_element ) {
+					if($count == 0){
+						$newAarray[$key] = self::transalate($setting_element);
+					}else{
+						$newAarray[$key] = ',' . self::transalate($setting_element);
+					}
+					$count ++;
 				}
-			}
-			if($setting['from_name']){}
-			if($setting['from_email']){}
-			if($setting['replay_To']){}
-			if($setting['subject']){}
-			if($setting['message']){}
+
 		}
-
-
 	}
 }
