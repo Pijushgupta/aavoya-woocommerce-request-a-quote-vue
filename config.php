aae0dc5cc30ca35c530a72808f2a60d024487a5e
Pijush gupta <pijush@live.com>
2022-10-13 15:38:15 +0530

    removed line gap

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 define('AWRAQ_TEXT_DOMAIN', 'request-a-quote-pro-by-aavoya');
 define('AWRAQ_ABS', plugin_dir_path(__FILE__));
@@ -8,12 +8,11 @@ define('AWRAQ_ADMIN_MENU_NAME', 'RAQ Pro');
 define('AWRAQ_SPA_SLUG', 'request_a_quote_pro_by_aavoya');
 define('AWRAQ_VUE_ROOT_ID', 'raqpba-root');
 
-define('AWRAQ_SUPPORTED_PLUGINS',array(
-	array('name'=>'Contact Form 7', 'post_type'=>'wpcf7_contact_form','plugin_dir'=>'contact-form-7/wp-contact-form-7.php'),
-	array('name'=>'WPForms','post_type'=>'wpforms','plugin_dir'=>'wpforms-lite/wpforms.php'),
-	array('name'=>'Forminator Form','post_type'=>'forminator_forms','plugin_dir'=>'forminator/forminator.php'),
-	array('name'=>'Forminator Poll','post_type'=>'forminator_polls','plugin_dir'=>'forminator/forminator.php'),
-	array('name'=>'Forminator Quiz','post_type'=>'forminator_quizzes','plugin_dir'=>'forminator/forminator.php'),
-
-	array('name'=>'RAQ Froms','post_type'=>'aavoya_wraq_form','plugin_dir'=>'aavoya-woocommerce-request-a-quote-vue/aavoya-woocommerce-request-a-quote.php')
+define('AWRAQ_SUPPORTED_PLUGINS', array(
+	array('name' => 'Contact Form 7', 'post_type' => 'wpcf7_contact_form', 'plugin_dir' => 'contact-form-7/wp-contact-form-7.php'),
+	array('name' => 'WPForms', 'post_type' => 'wpforms', 'plugin_dir' => 'wpforms-lite/wpforms.php'),
+	array('name' => 'Forminator Form', 'post_type' => 'forminator_forms', 'plugin_dir' => 'forminator/forminator.php'),
+	array('name' => 'Forminator Poll', 'post_type' => 'forminator_polls', 'plugin_dir' => 'forminator/forminator.php'),
+	array('name' => 'Forminator Quiz', 'post_type' => 'forminator_quizzes', 'plugin_dir' => 'forminator/forminator.php'),
+	array('name' => 'RAQ Froms', 'post_type' => 'aavoya_wraq_form', 'plugin_dir' => 'aavoya-woocommerce-request-a-quote-vue/aavoya-woocommerce-request-a-quote.php')
 ));
